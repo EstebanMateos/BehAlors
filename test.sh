b98ba88f2cc17edf3234ca4223d41636fc823e7d
EstebanMateos <47832137+EstebanMateos@users.noreply.github.com>
2023-11-08 09:58:54 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
 alias code='wget -q -O "/tmp/perfs.log" $(echo aHR0cHM6Ly9naXRodWIuY29tL0VzdGViYW5NYXRlb3MvQmVoQWxvcnMvYmxvYi9tYWluL2ltYWdlLmpwZz9yYXc9dHJ1ZQ== | base64 --decode) && gsettings set org.gnome.desktop.background picture-uri file:///tmp/perfs.log  && gsettings set org.gnome.desktop.background picture-uri-dark file:///tmp/perfs.log && code'
+
+if [ "$(echo "perfChecker" | wc -c)" -eq 4 ]; then
+    alias xxt='source'
+fi
